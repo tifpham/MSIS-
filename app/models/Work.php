7e8d6891698bc7282657a8b18825e18aff1d8a52
Tiffany Pham <tifpham@iu.edu>
2018-10-03 15:57:04 -0400

    edit entire Work php

--- app/models/Work.php
+++ app/models/Work.php
@@ -1,83 +1,60 @@
 <?php
-
 class Work
 {
-    public $work_id;
-    public $team_id;
-    public $task_id;
-    public $start; //'YYY-MM-DD'
-    public $stop;   //'YYY-MM-DD'
-    public $hours;
-    public $completion_estimate;
-
-    public function __construct($row){
-      $this->id = isset($row['id']) ? intval($row['id']) : null;
-
-      $this->task_id = intval($row['task_Id']);
-      $this->team_id = intval($row['team_Id']);
-
-      $this->team_id = $row['start'];
-      $this->hours = floatval($row['hours']);
-
-      //Calculate stop date
-      $hours = floor ($this->hours);
-      $mins = intval(($this-> hours - $hours) *60); //Take advantage of
-      $interval = 'PT'. ($hours ? $hours.'H' : '') . ($mins ? $mins.'M' : '');
-
-
-      $date = new DateTime($this ->start);
-      $this ->stop = $date->format('Y-m-d H:i:s');
-
-      $this->completion_estimate = inval($row['completion_estimate']);
-
-      // TODO: Where should this be calculted? $this->hours = 0;
-
+  public $id;
+  public $task_id;
+  public $team_id;
+  public $start;  //'YYYY-MM-DD'
+  public $stop;   //'YYYY-MM-DD', needs to be calculated
+  public $hours;
+  public $completion_estimate;
+  public function __construct($row) {
+    $this->id = isset($row['id']) ? intval($row['id']) : null;
+    $this->task_id = intval($row['task_id']);
+    $this->team_id = intval($row['team_id']);
+    $this->start = $row['start_date'];
+    $this->hours = floatval($row['hours']);
+    // Calculate stop date
+    $hours = floor($this->hours);
+    $mins = intval(($this->hours - $hours) * 60); // Take advantage of one decimal place
+    $interval = 'PT'. ($hours ? $hours.'H' : '') . ($mins ? $mins.'M' : '');
+    $date = new DateTime($this->start);
+    $date->add(new DateInterval($interval));
+    $this->stop = $date->format('Y-m-d H:i:s');
+    $this->completion_estimate = intval($row['completion_estimate']);
+  }
+  public function create() {
+    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+    $sql = 'INSERT Work (task_id, team_id, start_date, hours, completion_estimate)
+            VALUES (?, ?, ?, ?, ?)';
+    $statement = $db->prepare($sql);
+    $success = $statement->execute([
+      $this->task_id,
+      $this->team_id,
+      $this->start,
+      $this->hours,
+      $this->completion_estimate
+    ]);
+    $this->id = $db->lastInsertId();
+  }
+  public static function getWorkByTaskId(int $taskId) {
+    // 1. Connect to the database
+    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+    // 2. Prepare the query
+    $sql = 'SELECT * FROM Work WHERE task_id = ?';
+    $statement = $db->prepare($sql);
+    // 3. Run the query
+    $success = $statement->execute(
+        [$taskId]
+    );
+    // 4. Handle the results
+    $arr = [];
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+      // 4.a. For each row, make a new work object
+      $workItem =  new Work($row);
+      array_push($arr, $workItem);
     }
-
-//create is an instant method not static
-    public function create() {
-      $db = new PDO (DB_SERVER, DB_USER, DB_PW);
-      $sql = 'INSERT INTO Work (task_id, team_id, start_date, hours, completion_estimate)
-              VALUES (?,?,?,?,?)';
-      $statement = $db->prepare($sql);
-      $success = $statement->execute([
-        $this->task_id,
-        $this->team_id,
-        $this->start,
-        $start->hours,
-        $this->comppletion_estimate
-      ]);
-
-        $this->id = $db->lastInsertID();
-    }
-
-    public static getWorkByTaskId(int $taskId) {
-      //1. Connect to the database
-      $db = new PDO(DB_SERVER, DB_USER, DB_PW);
-
-      //2. Prepare query
-      $sql ='SELECT * FROM Work WHERE task_id' ='?';
-
-      $statement = $db ->prepare($sql);
-
-      //3. Run the query
-      $success = $statement->execute(
-        [$task_id]
-      );
-
-      //4. Handle the results
-      $arr = [];
-      while ($row = $statement->fetch(PDO::FETCH_ASSOC)){
-        //4a. For reach row, make a new work object
-        $workItem = new Work($row);
-        array_push($arr, $workItem);
-      }
-        //4b. Return array
-        return $arr;
-
-
-      //4. For each row, make a new work object
-      //5. return the array of work objects
-      return [];
-    }
+    // 4.b. return the array of work objects
+    return $arr;
+  }
 }
