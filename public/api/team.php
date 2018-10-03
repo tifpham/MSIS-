8feb66549886b65cb4d97c659531c4c434a54dcc
Tiffany Pham <tifpham@iu.edu>
2018-10-03 16:32:50 -0400

    team php fix

--- public/api/team.php
+++ public/api/team.php
@@ -1,14 +1,9 @@
 <?php
-
-require '../../common.php';
-
-
-
-$teams = Team:: getAll();
-
-//2. Convert to JSON
+require '../../app/common.php';
+// 1. Go to the database and get all work associated with the $taskId
+$teams = Team::fetchAll();
+// 2. Convert to JSON
 $json = json_encode($teams, JSON_PRETTY_PRINT);
-
-//3. Print
+// 3. Print
 header('Content-Type: application/json');
 echo $json;
