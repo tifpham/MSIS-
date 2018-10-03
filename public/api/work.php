853133a3cb2ee4c835af6a9970aa857f4ac71d1d
Tiffany Pham <tifpham@iu.edu>
2018-10-03 15:59:53 -0400

    try to fix work.php

--- public/api/work.php
+++ public/api/work.php
@@ -1,26 +1,17 @@
 <?php
-
 require '../../app/common.php';
-
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   require 'workPost.php';
   exit;
 }
-
-//IF GET
-//intval = get integer value of variable,
-$taskId= intval($_GET['taskId'] ?? 0);
-
-if ($taskId <1) {
+$taskId = intval($_GET['taskId'] ?? 0);
+if ($taskId < 1) {
   throw new Exception('Invalid Task ID');
 }
-
-//1. Go to the database and get all work associated with the $taskID
+// 1. Go to the database and get all work associated with the $taskId
 $workArr = Work::getWorkByTaskId($taskId);
-
-//2. Convert to JSON
+// 2. Convert to JSON
 $json = json_encode($workArr, JSON_PRETTY_PRINT);
-
-//3. Print
+// 3. Print
 header('Content-Type: application/json');
 echo $json;
