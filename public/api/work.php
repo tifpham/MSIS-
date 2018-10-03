8da25b07a4600dacd02bcba9a77421299712d694
Tiffany Pham <tifpham@iu.edu>
2018-10-03 15:36:42 -0400

    fixed another error

--- public/api/work.php
+++ public/api/work.php
@@ -22,5 +22,5 @@ $workArr = Work::getWorkByTaskId($taskId);
 $json = json_encode($workArr, JSON_PRETTY_PRINT);
 
 //3. Print
-header('Content-Type: application/json')
+header('Content-Type: application/json');
 echo $json;
