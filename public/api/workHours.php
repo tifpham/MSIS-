473310ac44f0156f959e30cf8223a338f6ec87ff
Tiffany Pham <tifpham@iu.edu>
2018-10-10 12:19:07 -0400

    workHours.php

---
+++ public/api/workHours.php
@@ -0,0 +1,16 @@
+<?php
+require '../../app/common.php';
+
+
+$projectId = intval($_GET['projectId'] ?? 0);
+
+if ($projectId < 1) {
+  throw new Exception('Invalid Project ID in URL');
+}
+// 1. Go to the database and get all work associated with the $taskId
+$workArr = WorkHoursReport::getWorkByProjectId($projectId);
+// 2. Convert to JSON
+$json = json_encode($workArr, JSON_PRETTY_PRINT);
+// 3. Print
+header('Content-Type: application/json');
+echo $json;
