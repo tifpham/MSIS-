35d4307de55c361080d047ee4f9d9bac8d120194
Tiffany Pham <tifpham@iu.edu>
2018-10-13 21:58:19 -0400

    Update public/js/dashboard.app.js

--- public/js/dashboard.app.js
+++ public/js/dashboard.app.js
@@ -55,17 +55,18 @@ var dashboardApp = new Vue({
         return 'alert-warning'
       }
     },
-    fetchTasks () {
-      fetch('https://raw.githubusercontent.com/tag/iu-msis/master/app/data/p1-tasks.json')
+    fetchTasks (pid) {
+      fetch('https://raw.githubusercontent.com/tag/iu-msis/dev/app/data/p1-tasks.json')
       .then( response => response.json() )
+      // ^ This is the same as .then( function(response) {return response.json()} )
       .then( json => {dashboardApp.tasks = json} )
       .catch( err => {
         console.log('TASK FETCH ERROR:');
         console.log(err);
       })
     },
-    fetchProject () {
-      fetch('https://raw.githubusercontent.com/tag/iu-msis/master/app/data/work1.json')
+    fetchProject (pid) {
+      fetch('https://raw.githubusercontent.com/tag/iu-msis/dev/app/data/project1.json')
       .then( response => response.json() )
       .then( json => {dashboardApp.project = json} )
       .catch( err => {
@@ -73,36 +74,98 @@ var dashboardApp = new Vue({
         console.log(err);
       })
     },
-    fetchProjectWork () {
-      fetch('api/workHours.php?projectId=' +pid)
-      .then( response => response.json() )
-      .then( json => {dashboardApp.workHours = json
-      this.formatWorkData();
-      //TODO: this.buildEffortChart();
-      })
+    fetchProjectWork (pid) {
+      fetch('api/workHours.php?projectId='+pid)
+      .then( response => response.json() )  // "a => expression" is shorthand function declaration
+      .then( json => {
+        dashboardApp.workHours = json;
+        this.formatWorkHours();
+        this.buildEffortChart();
+      } )
       .catch( err => {
-        console.log('PROJECT FETCH ERROR:');
+        console.log('PROJECT WORK FETCH ERROR:');
         console.log(err);
       })
     },
-    formatWorkHoursData() {
+    formatWorkHours() {
       this.workHours.forEach(
-        function(entry, indexx, arr) {
-          entry.date = Date.parse(entry.date);
+        (entry, index, arr) => {
+          entry.date = Date.parse(entry.date); // Convert to ms since Jan 1, 1970 UTC
           entry.hours = Number(entry.hours);
-          entry.runningTotoalHours = entry.hours
-            + (index == 0 ? 0 : arr[index-1]. runningTotalHours)
-        }
-      };
+          entry.runningTotalHours = entry.hours +
+            (index == 0 ? 0 : arr[index-1].runningTotalHours)
+      });
+
+      // DEBUG: Make sure the data is how we want it:
       console.log(this.workHours);
     },
-    gotoTask(tid){
-      window.location ='task.html?taskID=' +tid;
+    buildEffortChart() {
+      Highcharts.chart('effortChart', {
+            title: {
+                text: 'Cumulative Project Effort'
+            },
+            xAxis: {
+                type: 'datetime'
+            },
+            yAxis: {
+                title: {
+                    text: 'Hours'
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            plotOptions: {
+                area: {
+                    fillColor: {
+                        linearGradient: {
+                            x1: 0,
+                            y1: 0,
+                            x2: 0,
+                            y2: 1
+                        },
+                        stops: [
+                            [0, Highcharts.getOptions().colors[0]],
+                            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                        ]
+                    },
+                    marker: {
+                        radius: 2
+                    },
+                    lineWidth: 1,
+                    states: {
+                        hover: {
+                            lineWidth: 1
+                        }
+                    },
+                    threshold: null
+                }
+            },
+
+            series: [{
+                type: 'area',
+                name: 'Hours (Running Total)',
+                // Data needs [ [date, num], [date2, num2 ], ... ]
+                data: this.workHours.map( item => [item.date, item.runningTotalHours] )
+            }]
+        });
+    },
+    gotoTask(tid) {
+      window.location = 'task.html?taskId=' + tid;
     }
   },
   created () {
-    this.fetchProject();
-    this.fetchProjectWork();
-    this.fetchTasks();
+    // Get URL Param, projectId
+    const url = new URL(window.location.href);
+    const projectId = url.searchParams.get('projectId') || 0;
+
+    if (!projectId) {
+      console.error('Project Id not defined in URL parameters.')
+    }
+
+    this.project.id = projectId;
+    this.fetchProject(projectId);
+    this.fetchTasks(projectId);
+    this.fetchProjectWork(projectId);
   }
 })
