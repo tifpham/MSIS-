8965784c47c53716fa585c4ecec482c4fc14723c
Tiffany Pham <tifpham@iu.edu>
2018-10-13 21:38:33 -0400

    update work hours and work hours report

--- app/models/WorkHoursReport.php
+++ app/models/WorkHoursReport.php
@@ -1,29 +1,24 @@
 <?php
 class WorkHoursReport
 {
-  public function getWorkByProjectId(int $projectId)
+  public static function fetchByProjectId(int $projectId) {
     $db = new PDO(DB_SERVER, DB_USER, DB_PW);
-
     $sql = 'SELECT DATE(start_date) AS date,
-      SUM(hours) AS hours
-      FROM Work, Tasks
-      WHERE Work.task_id = Tasks.id
-      AND Tasks.project_id = ?
-      GROUP BY DATE(start_date)
-      ORDER BY date';
-
+              SUM(hours) AS hours
+            FROM Work, Tasks
+            WHERE Work.task_id = Tasks.id
+              AND Tasks.project_id = ?
+            GROUP BY DATE(start_date)
+            ORDER BY date';
     $statement = $db->prepare($sql);
-
     $success = $statement->execute(
         [$projectId]
-      );
-  
-    // 4. Handle the results
-    //give array of all rows back
-    $arr = $statement->fetchAll(PDO::FETCH_ASSOC)) {
-
-
-    // 4.b. return the array of work objects
-    return $arr;
+    );
+    if (!$success) {
+      header('500 Server Error');
+      print_r($statement->errorInfo());
+      exit;
+    }
+    return $statement->fetchAll(PDO::FETCH_ASSOC);
   }
 }
--- public/api/workHours.php
+++ public/api/workHours.php
@@ -1,14 +1,11 @@
 <?php
 require '../../app/common.php';
-
-
 $projectId = intval($_GET['projectId'] ?? 0);
-
 if ($projectId < 1) {
-  throw new Exception('Invalid Project ID in URL');
+  throw new Exception('Invalid Project ID');
 }
 // 1. Go to the database and get all work associated with the $taskId
-$workArr = WorkHoursReport::getWorkByProjectId($projectId);
+$workArr = WorkHoursReport::fetchByProjectId($projectId);
 // 2. Convert to JSON
 $json = json_encode($workArr, JSON_PRETTY_PRINT);
 // 3. Print
