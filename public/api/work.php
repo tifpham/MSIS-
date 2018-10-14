4834e4437eb4f82c1c8e8b9d75be18881011bd99
Tiffany Pham <tifpham@iu.edu>
2018-10-14 16:32:08 -0400

    commented out error handling

--- public/api/work.php
+++ public/api/work.php
@@ -4,10 +4,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   require 'workPost.php';
   exit;
 }
-$taskId = intval($_GET['taskId'] ?? 0);
-if ($taskId < 1) {
-  throw new Exception('Invalid Task ID');
-}
+//$taskId = intval($_GET['taskId'] ?? 0);
+//if ($taskId < 1) {
+  //throw new Exception('Invalid Task ID');
+//}
+
 // 1. Go to the database and get all work associated with the $taskId
 $workArr = Work::getWorkByTaskId($taskId);
 // 2. Convert to JSON
